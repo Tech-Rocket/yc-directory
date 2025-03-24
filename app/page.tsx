c1c584b9b107d68f6bcc9fa37dd10dd356b30e61
Ebee02 <beelawrence8@gmail.com>
2025-03-24 21:26:08 +0000

    Added the shadcn ui button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h1>Hello world</h1>
+
+      <Button>shadcn button</Button>
     </>
   );
 }
