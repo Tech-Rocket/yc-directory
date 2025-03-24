a6dc03615bc90013be16e92f9780e6a7a002399a
Ebee02 <beelawrence8@gmail.com>
2025-03-24 21:50:53 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello world</h1>
+    <div className="bg-black bg-home-img">
+      <main>
+        <div>
+          <h1>
+            Dan's Computer <br /> Repair Shop
+          </h1>
+          <address>
+            555 Gateway Lane <br />
+            Kansas City, KS 55555
+          </address>
+          <p>Open daily: 9am to 5pm</p>
 
-      <Button>shadcn button</Button>
-    </>
+          <Link href="tel:777777777"> 777-777-7777</Link>
+        </div>
+      </main>
+    </div>
   );
 }
